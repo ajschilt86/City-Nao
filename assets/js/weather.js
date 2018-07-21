9c86b5ec808cc8955334a7e3cf419a1122dc44f1
Cloud Strife <mokryan17@gmail.com>
2018-07-21 11:52:55 -0500

    working news api

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     var weatherKey = "f8c9acba8f1055fb160d7c22d0474eb4";
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + weatherKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInputLocation + "&appid=" + weatherKey;
 
     $.ajax({
         url: queryURL,
@@ -13,6 +13,7 @@ $(document).ready(function () {
         var minTemp = (response.main.temp_min - 273.15) * 1.8 + 32;
         var maxTemp = (response.main.temp_max - 273.15) * 1.8 + 32;
 
+        console.log(fahrenheit)
 
         $(".city").html("<h1>" + response.name + " Weather Details</h1>");
         $(".main").html("Main Temp (F) : " + fahrenheit);
