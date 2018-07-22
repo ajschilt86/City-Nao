e44f0b97c1903539efa43d65c1e296b35ca161b4
ajschilt <ajschilt86@gmail.com>
2018-07-22 11:02:33 -0500

    button working with city and date

--- assets/js/eventful.js
+++ assets/js/eventful.js
@@ -7,12 +7,17 @@ $(document).ready(function () {
 
     $("body").on("click", ".submit-button", function () {
         // variables
-        var userInputLocation = $(".location").val().trim();
+        var userInputLocation = $(".location-input").val().trim();
         var userInputDateStart = $("#start").val();
         var userInputDateEnd = $("#end").val();
 
+        console.log(userInputLocation);
         // url
         var queryURL = "https://api.eventful.com/json/events/search?app_key=tBSgjN5gKzmXQqxq&location=" + userInputLocation + "&date=" + userInputDateStart + "-" + userInputDateEnd + " ";
+
+        // var queryURL = "https://api.eventful.com/json/events/search?app_key=tBSgjN5gKzmXQqxq&location=" + userInputLocation + "";
+
+
         // var queryURL = "https://api.eventful.com/json/events/search?app_key=tBSgjN5gKzmXQqxq&location=chicago&date=today";
 
         // ajax call
@@ -22,28 +27,27 @@ $(document).ready(function () {
             // needed for this api
             dataType: 'jsonp',
             method: "GET"
-        })
+        }).then(function (response) {
+            for (var i = 0; i < 10; i++) {
 
-            .then(function (response) {
-                for (var i = 0; i < 10; i++) {
+                console.log(response);
+                console.log(response.events.event[i].title);
+                console.log(response.events.event[i].venue_name);
+                console.log(response.events.event[i].city_name);
+                console.log(response.events.event[i].venue_address);
+                console.log(response.events.event[i].description);
+                console.log(response.events.event[i].venue_url);
 
-                    console.log(response);
-                    console.log(response.events.event[i].title);
-                    console.log(response.events.event[i].venue_name);
-                    console.log(response.events.event[i].city_name);
-                    console.log(response.events.event[i].venue_address);
-                    console.log(response.events.event[i].description);
-                    console.log(response.events.event[i].venue_url);
+                $(".events-display").append("<div class='events-output-title'>Title:" + response.events.event[i].title + "</div>");
+                $(".events-display").append("<div class='events-output-date'>Date:" + response.events.event[i].start_time + "</div>");
+                $(".events-display").append("<div class='events-output-venueName'>Venue Name:" + response.events.event[i].venue_name + "</div>");
+                $(".events-display").append("<div class='events-output-cityName'>City Name:" + response.events.event[i].city_name + "</div>");
+                $(".events-display").append("<div class='events-output-description'>Description:" + response.events.event[i].description + "</div>");
+                $(".events-display").append("<div class='events-output-venueAddress'>Venue Address:" + response.events.event[i].venue_address + "</div>");
+                $(".events-display").append("<div class='events-output-venueURL'>Venue URL:<a href='" + response.events.event[i].venue_url + "'>Click here</a></div>");
 
-                    $(".events-display").append("<div class='events-output-title'>Title:" + response.events.event[i].title + "</div>");
-                    $(".events-display").append("<div class='events-output-venueName'>Venue Name:" + response.events.event[i].venue_name + "</div>");
-                    $(".events-display").append("<div class='events-output-cityName'>City Name:" + response.events.event[i].city_name + "</div>");
-                    $(".events-display").append("<div class='events-output-description'>Description:" + response.events.event[i].description + "</div>");
-                    $(".events-display").append("<div class='events-output-venueAddress'>Venue Address:" + response.events.event[i].venue_address + "</div>");
-                    $(".events-display").append("<div class='events-output-venueURL'>Venue URL:" + response.events.event[i].venue_url + "</div>");
-
-                }
-            });
+            }
+        });
 
     });
 
