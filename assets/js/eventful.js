696a4313aa85429e9d6e158664ddcba8e2bc99ea
ajschilt <ajschilt86@gmail.com>
2018-07-23 18:49:55 -0500

    added missing div tag

--- assets/js/eventful.js
+++ assets/js/eventful.js
@@ -31,18 +31,15 @@ $(document).ready(function () {
                 // console.log(response.events.event[i].description);
                 // console.log(response.events.event[i].venue_url);
 
-                $(".events-display").append(
-                      "<section class='events-output'>Title:" + response.events.event[i].title + "</div>"
+                $(".events-display").html(
+                      "<section class='events-output'><div>Title:" + response.events.event[i].title + "</div>"
                     + "<div>Date:" + response.events.event[i].start_time + "</div>"
                     + "<div>Venue Name:" + response.events.event[i].venue_name + "</div>"
                     + "<div>City Name:" + response.events.event[i].city_name + "</div>"
                     + "<div>Description:" + response.events.event[i].description + "</div>"
                     + "<div>Venue Address:" + response.events.event[i].venue_address + "</div>"
                     + "<div>Venue URL:<a href='" + response.events.event[i].venue_url + "'>Click here</a></div></section>");
-
             }
         });
-
     });
-
 });
