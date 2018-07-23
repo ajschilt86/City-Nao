6ad00e2f38aa92612a58c3b30c03b21fce4216bb
ajschilt <ajschilt86@gmail.com>
2018-07-23 18:23:35 -0500

    fixed divs

--- assets/js/eventful.js
+++ assets/js/eventful.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
 
-    // var APIKey = tBSgjN5gKzmXQqxq;
-
-
-
-
     $("body").on("click", ".submit-button", function (event) {
         // variables
         event.preventDefault();
@@ -12,15 +7,11 @@ $(document).ready(function () {
         var userInputLocation = $(".location-input").val().trim();
         var userInputDateStart = $("#start").val();
         var userInputDateEnd = $("#end").val();
+        // var APIKey = tBSgjN5gKzmXQqxq;
 
         console.log(userInputLocation);
         // url
         var queryURL = "https://api.eventful.com/json/events/search?app_key=tBSgjN5gKzmXQqxq&location=" + userInputLocation + "&date=" + userInputDateStart + "-" + userInputDateEnd + " ";
-
-        // var queryURL = "https://api.eventful.com/json/events/search?app_key=tBSgjN5gKzmXQqxq&location=" + userInputLocation + "";
-
-
-        // var queryURL = "https://api.eventful.com/json/events/search?app_key=tBSgjN5gKzmXQqxq&location=chicago&date=today";
 
         // ajax call
 
@@ -32,7 +23,7 @@ $(document).ready(function () {
         }).then(function (response) {
             for (var i = 0; i < 10; i++) {
 
-                console.log(response);
+                // console.log(response);
                 // console.log(response.events.event[i].title);
                 // console.log(response.events.event[i].venue_name);
                 // console.log(response.events.event[i].city_name);
@@ -40,13 +31,14 @@ $(document).ready(function () {
                 // console.log(response.events.event[i].description);
                 // console.log(response.events.event[i].venue_url);
 
-                $(".events-display").append("<div class='events-output'><div class='events-output-title'>Title:" + response.events.event[i].title + "</div>");
-                $(".events-display").append("<div class='events-output-date'>Date:" + response.events.event[i].start_time + "</div>");
-                $(".events-display").append("<div class='events-output-venueName'>Venue Name:" + response.events.event[i].venue_name + "</div>");
-                $(".events-display").append("<div class='events-output-cityName'>City Name:" + response.events.event[i].city_name + "</div>");
-                $(".events-display").append("<div class='events-output-description'>Description:" + response.events.event[i].description + "</div>");
-                $(".events-display").append("<div class='events-output-venueAddress'>Venue Address:" + response.events.event[i].venue_address + "</div>");
-                $(".events-display").append("<div class='events-output-venueURL'>Venue URL:<a href='" + response.events.event[i].venue_url + "'>Click here</a></div></div>");
+                $(".events-display").append(
+                      "<section class='events-output'>Title:" + response.events.event[i].title + "</div>"
+                    + "<div>Date:" + response.events.event[i].start_time + "</div>"
+                    + "<div>Venue Name:" + response.events.event[i].venue_name + "</div>"
+                    + "<div>City Name:" + response.events.event[i].city_name + "</div>"
+                    + "<div>Description:" + response.events.event[i].description + "</div>"
+                    + "<div>Venue Address:" + response.events.event[i].venue_address + "</div>"
+                    + "<div>Venue URL:<a href='" + response.events.event[i].venue_url + "'>Click here</a></div></section>");
 
             }
         });
